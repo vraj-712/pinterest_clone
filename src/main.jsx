d812bf467c9ff2b72d6c95072d1a0e9ebcd507ed
darshi-90 <darshipujara90@gmail.com>
2024-04-07 22:21:19 +0530

    Home Page Layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Explore from './pages/Explore.jsx'
-<<<<<<< HEAD
 import Home from './pages/Home.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <Explore /> */}
-    <Home/>
-=======
 import Navbar from './components/Navbar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Home/>
     <Navbar />
     <Explore />
->>>>>>> main
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
