69a0676c66483280c23b811c7a49706af2e63ac9
vraj <patelvraju07@gmail.com>
2024-04-14 22:43:42 +0530

    Small Change In Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <>
     <div className="columns-1 xs:columns-1 sm:columns-2 md:columns-4 lg:columns-6 [&>img:not(:first-child)]:mt-6 p-7 ">
-        <img src="https://i.pinimg.com/236x/c6/57/51/c6575187a14599b7b3531e8f3e046855.jpg" alt="Image 1" />
-        <img src="https://i.pinimg.com/236x/7c/c0/b3/7cc0b3fe8f9dc0ca60acbb49e4a46af1.jpg" alt="Image 1"   />
-        <img src="https://i.pinimg.com/236x/e8/aa/b0/e8aab0a8a3cfc9fab339a4b288b872c3.jpg" alt="Image 1" />
-        <img src="https://i.pinimg.com/236x/c6/57/51/c6575187a14599b7b3531e8f3e046855.jpg" alt="Image 1" />
-        <img src="https://i.pinimg.com/236x/7c/c0/b3/7cc0b3fe8f9dc0ca60acbb49e4a46af1.jpg" alt="Image 1" />
+        <img src="https://i.pinimg.com/236x/c6/57/51/c6575187a14599b7b3531e8f3e046855.jpg" alt="Image 1"/>
+        <img src="https://i.pinimg.com/236x/7c/c0/b3/7cc0b3fe8f9dc0ca60acbb49e4a46af1.jpg" alt="Image 1"/>
+        <img src="https://i.pinimg.com/236x/e8/aa/b0/e8aab0a8a3cfc9fab339a4b288b872c3.jpg" alt="Image 1"/>
+        <img src="https://i.pinimg.com/236x/c6/57/51/c6575187a14599b7b3531e8f3e046855.jpg" alt="Image 1"/>
+        <img src="https://i.pinimg.com/236x/7c/c0/b3/7cc0b3fe8f9dc0ca60acbb49e4a46af1.jpg" alt="Image 1"/>
         <img src="https://i.pinimg.com/236x/3a/1c/8c/3a1c8ce2556e251ed29752fd7ab095a0.jpg" alt="Image 1"/>
         <img src="https://i.pinimg.com/236x/c6/57/51/c6575187a14599b7b3531e8f3e046855.jpg" alt="Image 1"/>
         <img src="https://i.pinimg.com/236x/7c/c0/b3/7cc0b3fe8f9dc0ca60acbb49e4a46af1.jpg" alt="Image 1"/>
         <img src="https://i.pinimg.com/236x/3a/1c/8c/3a1c8ce2556e251ed29752fd7ab095a0.jpg" alt="Image 1"/>
+        <img src="https://i.pinimg.com/236x/3a/1c/8c/3a1c8ce2556e251ed29752fd7ab095a0.jpg" alt="Image 1"/>
         
-        <img src="https://i.pinimg.com/236x/3a/1c/8c/3a1c8ce2556e251ed29752fd7ab095a0.jpg" alt="Image 1" />
         <img src="https://source.unsplash.com/bYuI23mnmDQ"/>
         <img src="https://source.unsplash.com/Nllx4R-2c3o"/>
         <img src="https://source.unsplash.com/lp40q07DIe0"/>
